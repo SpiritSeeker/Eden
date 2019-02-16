cae353fe084b527e5b9f08185a49cbdcb00a6d89
SpiritSeeker <sanjeet029@gmail.com>
2019-02-16 12:00:24 +0530

    Add confirmation for close and serious mode on

--- eden_main.py
+++ eden_main.py
@@ -2,33 +2,38 @@ import sys
 from PyQt5 import QtWidgets
 
 class Window(QtWidgets.QMainWindow):
-	"""docstring for Window"""
+
 	def __init__(self):
 		super(Window, self).__init__()
 		self.setGeometry(50,50,500,300)
-		self.setWindowTitle("Dayummmm. Music!!!")
+		self.setWindowTitle("Eden")
 
-		extractAction = QtWidgets.QAction('&New Shit',self)
+		extractAction = QtWidgets.QAction('&Open File',self)
 		extractAction.setShortcut("Ctrl+N")
-		extractAction.setStatusTip('Open new music')
-
-		self.statusBar()
 
 		mainMenu = self.menuBar()
-		stuffMenu = mainMenu.addMenu('&Good Stuff')
-		stuffMenu.addAction(extractAction)
+		fileMenu = mainMenu.addMenu('&File')
+		fileMenu.addAction(extractAction)
 
-		extractAction = QtWidgets.QAction('&Eh. Quit',self)
+		extractAction = QtWidgets.QAction('&Exit',self)
 		extractAction.setShortcut('Ctrl+Q')
-		extractAction.setStatusTip('I\'mma fool')
-		extractAction.triggered.connect(sys.exit)
-		stuffMenu.addAction(extractAction)
-
+		extractAction.triggered.connect(self.close_application)
+		fileMenu.addAction(extractAction)
 
 		self.home()
 		
+	def close_application(self):
+		choice = QtWidgets.QMessageBox.question(self, 'Quit',
+			'Are you sure you want to exit?',
+			QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No)
+		if choice == QtWidgets.QMessageBox.Yes:
+			sys.exit()
+		else:
+			pass	
+
+
 	def home(self):
-		self.showMaximized()	
+		self.showMaximized()
 
 
 def main():
