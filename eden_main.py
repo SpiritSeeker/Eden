70cc3d90e9711760a17db29499bc2f8a739fcbdb
SpiritSeeker <sanjeet029@gmail.com>
2019-02-16 14:43:49 +0530

    Added bare homepage

--- eden_main.py
+++ eden_main.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtGui, QtCore
 
 class Window(QtWidgets.QMainWindow):
 
@@ -7,6 +7,7 @@ class Window(QtWidgets.QMainWindow):
 		super(Window, self).__init__()
 		self.setGeometry(50,50,500,300)
 		self.setWindowTitle("Eden")
+		self.screenShape = QtWidgets.QDesktopWidget().screenGeometry()
 
 		extractAction = QtWidgets.QAction('&Open File',self)
 		extractAction.setShortcut("Ctrl+N")
@@ -33,14 +34,43 @@ class Window(QtWidgets.QMainWindow):
 
 
 	def home(self):
+		oImage = QtGui.QImage('imgs/neon.jpg')
+		palette = QtGui.QPalette()
+		palette.setBrush(10, QtGui.QBrush(oImage))
+		self.setPalette(palette)
+
+		self.label = QtWidgets.QLabel(self)
+		self.label.setText("<span style='font-size:100pt;text-align:center;color:white;'>EDEN</span>")
+		f = self.label.font()
+		f.setStyleStrategy(QtGui.QFont.PreferAntialias)
+		self.label.setFont(f)
+		self.label.resize(self.label.minimumSizeHint())
+		self.label.move(int((self.screenShape.width()-self.label.frameGeometry().width())/2), int((self.screenShape.height()-self.label.frameGeometry().height())/3))
+		self.unfade(self.label)
 		self.showMaximized()
 
+	def fade(self, widget):
+		self.effect = QtWidgets.QGraphicsOpacityEffect()
+		widget.setGraphicsEffect(self.effect)
 
-def main():
-	app = QtWidgets.QApplication(sys.argv)
-	GUI = Window()
-	sys.exit(app.exec_())
+		self.animation = QtCore.QPropertyAnimation(self.effect, b"opacity")
+		self.animation.setDuration(1000)
+		self.animation.setStartValue(1)
+		self.animation.setEndValue(0)
+		self.animation.start()
+
+	def unfade(self, widget):
+		self.effect = QtWidgets.QGraphicsOpacityEffect()
+		widget.setGraphicsEffect(self.effect)
+
+		self.animation = QtCore.QPropertyAnimation(self.effect, b"opacity")
+		self.animation.setDuration(1000)
+		self.animation.setStartValue(0)
+		self.animation.setEndValue(1)
+		self.animation.start()	
 
 
 if __name__ == "__main__":
-	main()	
+	app = QtWidgets.QApplication(sys.argv)
+	GUI = Window()
+	sys.exit(app.exec_())
