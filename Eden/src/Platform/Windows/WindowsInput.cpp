095ab4523518ac0cd2ea39c5ce41e1cbae1c3acf
SpiritSeeker <sanjeet029@gmail.com>
2020-03-28 23:07:27 +0530

    Added Input

---
+++ Eden/src/Platform/Windows/WindowsInput.cpp
@@ -0,0 +1,47 @@
+#include "edenpch.h"
+#include "WindowsInput.h"
+
+#include "Eden/Application.h"
+#include <GLFW/glfw3.h>
+
+namespace Eden {
+
+	Input* Input::s_Instance = new WindowsInput();
+
+	bool WindowsInput::IsKeyPressedImpl(int keycode)
+	{
+		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
+		auto state = glfwGetKey(window, keycode);
+		return state == GLFW_PRESS || state == GLFW_REPEAT;
+	}
+
+	bool WindowsInput::IsMouseButtonPressedImpl(int button)
+	{
+		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
+		auto state = glfwGetMouseButton(window, button);
+		return state == GLFW_PRESS;
+	}
+
+	std::pair<float, float> WindowsInput::GetMousePositionImpl()
+	{
+		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
+		double xpos, ypos;
+		glfwGetCursorPos(window, &xpos, &ypos);
+
+		return { (float)xpos, (float)ypos };
+	}
+
+	float WindowsInput::GetMouseXImpl()
+	{
+		auto pos = GetMousePositionImpl();
+		return pos.first;
+	}
+
+	float WindowsInput::GetMouseYImpl()
+	{
+		auto pos = GetMousePositionImpl();
+		return pos.second;
+	}
+
+
+}
